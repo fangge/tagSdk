c82200af048584f7b2bc3cba83c319fb53b49705
Mrangge <mickshifangge@gmail.com>
2017-07-21 18:33:54 +0800

    添加标签组件介绍

---
+++ README.md
@@ -0,0 +1,82 @@
+# 标签组件
+
+最近在改版的时候，遇到一个标签插入的小需求，没错，就像下图显示的那样：
+
+![标签插入](tag.jpg "标签输入")
+
+刚开始想着不就一个简单的标签组件，很简单的嘛，可是后来发现越简单的东西，可能需要考虑的用户体验，还有本身组件的可扩展性都是需要仔细斟酌的。
+
+在踩了坑之后，决定把这个标签逻辑重构，形成单独的标签组件，以方便日后的复用
+
+## 标签组件功能
+
+在决定做成组件之前，又重新看了之前的代码，然后思考了一下一个标签组件到底需要什么功能，才能保证这个组件的可扩展性高，用户体验又好呢，于是就规划了一下这个组件可以有的功能点：
+
+1. 标签插入去重
+2. 可设定空格或回车输入
+3. 限制可插入标签数
+4. 是否有需屏蔽标签
+5. 是否有推荐标签
+6. 删除标签方法（回退、点击删除）
+7. 各种提示的自定义
+
+## 标签组件基本架构
+
+```javascript
+(function (window) {
+        var TagSdk = function () {
+            return;
+        };
+        /**
+         * @description 扩展参数对比
+         * @param {object} target - 目标对象
+         * @param {object} source - 源对象
+         * @returns {object} - 合成的对象
+         */
+        TagSdk.prototype.extend = function (target, source) {
+              for (var p in source) {
+                  if (source.hasOwnProperty(p)) {
+                      target[p] = source[p];
+                  }
+              }
+              return target;
+          };
+        TagSdk.prototype.xxx = function() {
+          
+        };
+})(window, document);
+```
+
+## 组件说明
+
+- 开始使用，默认参数如下
+
+```javascript
+    /**
+     * 初始化
+     * @param opts 参数
+     * @param {number} insertType - 输入标签的方式，13为回车，32为空格，如果选择32，则输入的标签不能允许有空格
+     * @param {number} maxTag - 允许插入的标签数量
+     * @param {string} maxTagTip - 可插入标签数量提示容器
+     * @param {string} alertTag - 标签相关提示容器，如果为alert，则直接弹出系统提示框
+     * @param {string} hotTag - 推荐标签
+     * @param {string} inputTag - 输入标签为空的提示
+     * @param {string} lengthError - 超过限制标签数量提示
+     * @param {string} exitError - 标签去重提示
+     */
+    TagSdk.tagInit({
+            insertType:13,
+            maxTag:20,
+            maxTagTip:'tag-num',
+            alertTag:'tag-alert',
+            hotTag:'tag-hot',
+            inputTag:'tag-input',
+            inputError:'请输入标签',
+            lengthError:'已超过限制标签数',
+            exitError:'已存在标签'
+    });
+```
+
+   - 删除功能
+   
+   按回退键，或直接点击标签进行删除
---
+++ tagInsert.js
@@ -0,0 +1,113 @@
+(function (window) {
+    var TagSdk = function () {
+        return;
+    };
+    /**
+     * @description 扩展参数对比
+     * @param {object} target - 目标对象
+     * @param {object} source - 源对象
+     * @returns {object} - 合成的对象
+     */
+    TagSdk.prototype.extend = function (target, source) {
+        for (var p in source) {
+            if (source.hasOwnProperty(p)) {
+                target[p] = source[p];
+            }
+        }
+        return target;
+    };
+    /**
+     * 初始化
+     * @param opts 参数
+     * @param {number} insertType - 输入标签的方式，13为回车，32为空格，如果选择32，则输入的标签不能允许有空格
+     * @param {number} maxTag - 允许插入的标签数量
+     * @param {number} maxTagLength - 标签字数限制
+     * @param {string} maxTagTip - 可插入标签数量提示容器
+     * @param {string} alertTag - 标签相关提示容器，如果为alert，则直接弹出系统提示框
+     * @param {string} hotTag - 推荐标签
+     * @param {string} inputTag - 输入标签为空的提示
+     * @param {string} lengthError - 超过限制标签数量提示
+     * @param {string} exitError - 标签去重提示
+     * @param {bollean} emptyFlag - 是否必须填标签
+     */
+    TagSdk.prototype.tagInit = function(opts) {
+        var t = this;
+        t.defaults = {
+            insertType:13,
+            maxTag:20,
+            maxTagLength:20,
+            maxTagTip:'tag-num',
+            alertTag:'tag-alert',
+            hotTag:'tag-hot',
+            inputTag:'tag-input',
+            inputError:'请输入标签',
+            numError:'已超过限制标签数',
+            exitError:'已存在标签',
+            emptyFlag:false
+        };
+        t.extend(t.defaults,opts);
+
+        var tag_num = document.querySelector('[data-role='+t.defaults.maxTagTip+']'),
+            tag_list = document.querySelectorAll('a.tag-list'),
+            tag_input = document.querySelector('[data-role='+t.defaults.inputTag+']'),
+              tag_hot = document.querySelector('[data-role='+t.defaults.hotTag+']');
+
+        //初始化
+        tag_num.innerHTML = t.defaults.maxTag - tag_list.length;
+        if(t.defaults.insertType == 13){
+            tag_input.setAttribute('placeholder','输入标签内容后按回车添加')
+        }else{
+            tag_input.setAttribute('placeholder','输入标签内容后按空格添加')
+        }
+        t.insertTag();
+    };
+    /**
+     * 插入标签
+     * @param input
+     */
+    TagSdk.prototype.insertTag = function () {
+        var t = this,
+            tag_list = document.querySelectorAll('a.tag-list'),
+            tag_input = document.querySelector('[data-role='+t.defaults.inputTag+']');
+        tag_input.addEventListener('keyup',function (e) {
+            var event = e|| window.e,
+                keyword,
+                el = this,
+                insertFlag = true;
+            if(e.keyCode == t.defaults.insertType){
+                keyword = el.value;
+                if(keyword != ""){
+
+                    if(tag_list.length >0){
+                        //如果已有标签，则去重
+                       [].forEach.call(tag_list,function (i) {
+                           if(i.innerText == keyword)insertFlag = false;
+                       })
+                    }
+
+                    if(keyword.length>t.defaults.maxTagLength){
+                        t.userAlert("标签字数超过"+t.defaults.maxTagLength+'字，请重新输入！');
+                    }
+                }
+            }
+        })
+    }
+    /**
+     * 用户提示
+     * @param str
+     */
+    TagSdk.prototype.userAlert = function (str) {
+        var t = this;
+        if(t.defaults.alertTag == "alert"){
+            alert(str);
+        }else{
+            var  tag_alert = document.querySelector('[data-role='+t.defaults.alertTag+']');
+            tag_alert.innerHTML = str;
+        }
+
+    }
+
+
+
+    window.TagSdk = new TagSdk();
+})(window, document);
